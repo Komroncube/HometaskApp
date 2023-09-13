96b3edeacacf716e3c480aacaa488f7a851f5c99
Komron <komronabdujabbor@gmail.com>
2023-09-13 12:49:37 +0500

    added function to parse string to int

--- Hometask/SolutionFor_12_09_2023.cs
+++ Hometask/SolutionFor_12_09_2023.cs
@@ -46,8 +46,10 @@ namespace Hometask
         public void Problem2()
         {
             Console.Write("Natural kiriting: ");
-            bool check = int.TryParse(Console.ReadLine(), out int number);
+            int number;
+            bool check = isNumber(out number);
             int newnumber = 0, tmp = number;
+            if (check)
             if (check)
             {
                 while (number > 0 && check)
@@ -73,9 +75,8 @@ namespace Hometask
         public void Problem3()
         {
             Console.Write("Natural kiriting: ");
-            bool check = int.TryParse(Console.ReadLine(), out int number);
-
-            int tmp = 0;
+            int number, tmp = 0;
+            bool check = isNumber(out number);
             if (check)
             {
                 int sign = 1;
@@ -100,19 +101,27 @@ namespace Hometask
         public void Problem4()
         {
             Console.Write("Natural kiriting: ");
-            bool check = int.TryParse(Console.ReadLine(), out int number);
-            for (int i = 0; i < number-1; i++) 
+            int number;
+            bool check = isNumber(out number);
+            if (check)
             {
-                string blank = new string(' ', number - i);
-                string stars = new string ('*', i);
-                Console.WriteLine(blank+stars+"*"+stars);
+                for (int i = 0; i < number-1; i++) 
+                {
+                    string blank = new string(' ', number - i);
+                    string stars = new string ('*', i);
+                    Console.WriteLine(blank+stars+"*"+stars);
+                }
+                for (int i = 1; i < number+1; i++)
+                {
+                    string blank = new string(' ', i);
+                    string stars = new string('*', number-i);
+                    Console.WriteLine(blank + stars + "*" + stars);
+                }
             }
-            for (int i = 1; i < number+1; i++)
-            {
-                string blank = new string(' ', i);
-                string stars = new string('*', number-i);
-                Console.WriteLine(blank + stars + "*" + stars);
-            }
+        }
+        private bool isNumber(out int number)
+        {
+            return int.TryParse(Console.ReadLine(), out number);
         }
     }
 }
