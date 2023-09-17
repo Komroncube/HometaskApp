73f83fd01978106be1087c93259cfda75f84869d
Komron <komronabdujabbor@gmail.com>
2023-09-17 21:25:56 +0500

    collections done

---
+++ Hometask/Collections/ExtensionMethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask.Collections
+{
+    public static class ExtensionMethod
+    {
+        public static void YangiExtensionMethod(this int[][] jaggedArray, int index, int value)
+        {
+            
+            Array.Resize(ref jaggedArray[index], jaggedArray[index].Length+1);
+            jaggedArray[index][jaggedArray[index].Length - 1] = value;
+        }
+
+        //returnsiz ishlatgan paytim reference ni qabul qilib olmadi
+        public static int[][] YangiExtensionMethod(this int[][] jaggedArray, int value)
+        {
+            Array.Resize(ref jaggedArray, jaggedArray.Length+1);
+            jaggedArray[jaggedArray.Length - 1] = new int[] { value };
+            return jaggedArray;
+        }
+    }
+}
--- Hometask/Program.cs
+++ Hometask/Program.cs
@@ -1,12 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Hometask;
+using Hometask.Collections;
 
 //Console.WriteLine("Hello, World!");
-var solution = new SolutionFor_12_09_2023();
+/*var solution = new SolutionFor_12_09_2023();
 solution.Problem1();
 solution.Problem2();
 solution.Problem3();
 solution.Problem4();
 solution.Problem1_New();
-Console.WriteLine(solution.MaximumNumber("132",new int[] { 9, 8, 5, 0, 3, 6, 4, 2, 6, 8}));
+Console.WriteLine(solution.MaximumNumber("132",new int[] { 9, 8, 5, 0, 3, 6, 4, 2, 6, 8}));*/
 
+int[][] jaggedArray = new int[2][];
+jaggedArray[0] = new int[] { 31 };
+jaggedArray[1] = new int[] { 28, 29 };
+jaggedArray.YangiExtensionMethod(1, 7);
+jaggedArray = jaggedArray.YangiExtensionMethod(8);
+
+
+int len = jaggedArray.Length;
+for(int i = 0; i < len; i++)
+{
+    foreach (int j in jaggedArray[i])
+    {
+        Console.Write(j + " ");
+    }
+    Console.WriteLine();
+
+}
